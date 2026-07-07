7e048c0c0b7591e83b84bd3e064315621d0d0c11
Jatin <jatin05narayan@gmail.com>
2026-07-07 19:07:28 +0530

    Add files via upload

---
+++ Project Folder/scripts/slime.gd
@@ -0,0 +1,16 @@
+extends Node2D
+const speed = 60
+var dir = 1
+@onready var ray_castleft: RayCast2D = $RayCastleft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_castright: RayCast2D = $RayCastright
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_castright.is_colliding() :
+		dir = -1
+		animated_sprite_2d.flip_h = true
+	if ray_castleft.is_colliding() :
+		dir = 1
+		animated_sprite_2d.flip_h = false
+	position.x += dir * speed * delta
